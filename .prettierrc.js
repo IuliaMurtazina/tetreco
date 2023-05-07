21e665662552a8e07c36984b21cb5c29071eee1e
Iulia Murtazina <iulia.murtazina@gmail.com>
2023-05-07 15:20:20 +0300

    Eslint, prettier, husky pre-commit are added. Switch and Input components are created

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  singleQuote: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "all",
+  singleAttributePerLine: true,
+};
